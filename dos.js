6e495649a16927225e829b88439f0303f6776d59
Bettsy Iriarte Gutierrez <birigu170299@gmail.com>
2024-08-31 15:37:44 -0300

    init: inicio de la aplicacion

---
+++ dos.js
@@ -0,0 +1,1 @@
+const samuel2 = 'holaaa';
---
+++ index.js
@@ -0,0 +1,5 @@
+const persona = (nombre, edad, sexo) =>  `nombre: ${nombre.toUpperCase()}, edad: ${edad}, sexo: ${sexo}`;
+
+
+console.log(persona('samuel', 34, 'masculino'));
+
